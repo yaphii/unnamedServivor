5dcfb2a80de11f6e791a904be68052cdba6c7d4c
yaphii <141389064+yaphii@users.noreply.github.com>
2023-10-21 01:56:05 +0900

    Hud
    타이머, 텍스트

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -8,7 +8,7 @@ public class HUD : MonoBehaviour
     //다루게 될 enum을 미리 선언 
     public enum InfoType {Exp, Level, Kill, Time, Health};    public InfoType type;
     Text myText;
-    Slider mySlider;	private void Awake()	{        myText = GetComponent<Text>();		mySlider = GetComponent<Slider>();							}	// Start is called before the first frame update														void Start()
+    Slider mySlider;	private void Awake()	{        myText = GetComponent<Text>();		mySlider = GetComponent<Slider>();							}	// Start is called before the first frame update	void Start()
     {
         
     }
@@ -17,5 +17,5 @@ public class HUD : MonoBehaviour
     void Update()
     {
         
-    }	private void LateUpdate()	{        switch (type)        {            case InfoType.Exp:                float curExp = GameManager.instance.exp;				float maxExp = GameManager.instance.nextExp[GameManager.instance.level];                mySlider.value = curExp / maxExp;				break;            case InfoType.Health:                break;            case InfoType.Level:				myText.text = string.Format("Lv.{0:F0}", GameManager.instance.level);				break;            case InfoType.Kill:                myText.text = string.Format("{0:F0}", GameManager.instance.kill);                break;            case InfoType.Time:                break;            default:                break;        }         }
+    }	private void LateUpdate()	{        switch (type)        {            case InfoType.Exp:                float curExp = GameManager.instance.exp;				float maxExp = GameManager.instance.nextExp[GameManager.instance.level];                mySlider.value = curExp / maxExp;				break;            case InfoType.Health:                break;            case InfoType.Level:				myText.text = string.Format("Lv.{0:F0}", GameManager.instance.level);				break;            case InfoType.Kill:                myText.text = string.Format("{0:F0}", GameManager.instance.kill);                break;            case InfoType.Time:                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;                int min = Mathf.FloorToInt(remainTime / 60);                int sec = Mathf.FloorToInt(remainTime % 60);                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);                break;            default:                break;        }         }
 }
