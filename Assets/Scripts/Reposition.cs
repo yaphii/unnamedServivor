84faed793d5d261d9a362a959adca782f4eb9f0a
yaphii <141389064+yaphii@users.noreply.github.com>
2024-01-11 15:43:12 +0900

    로직보완
    사용자 입력값이 아닌 오브젝트 위치값 만을 이용해 무한맵, 몬스터 재배치 보완

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -5,8 +5,58 @@ using UnityEngine;
 public class Reposition : MonoBehaviour
 {
 	Collider2D coll;
-	private void Awake()	{		coll = GetComponent<Collider2D>();	}
+	private void Awake()
+	{
+		coll = GetComponent<Collider2D>();
+	}
 
-	private void OnTriggerExit2D(Collider2D collision)	{		if (!collision.CompareTag("Area"))			return;		Vector3 playerPos = GameManager.instance.player.transform.position;		Vector3 myPos = transform.position  ;		float diffX = Mathf.Abs(playerPos.x - myPos.x); 		float diffY = Mathf.Abs(playerPos.y - myPos.y);		Vector3 playerDir= GameManager.instance.player.inputVec;		float dirX = playerDir.x < 0 ? -1 : 1;		float dirY = playerDir.y < 0 ? -1 : 1;		switch (transform.tag)		{			case "Ground":				if (diffX > diffY)				{					transform.Translate(Vector3.right * dirX * 40);				}				else if(diffX < diffY)				{					transform.Translate(Vector3.up* dirY * 40);				}				break;			case "Enemy":				if (coll.enabled)				{					transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));										}				break;			default:				break;		}		}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (!collision.CompareTag("Area"))
+			return;
+
+		Vector3 playerPos = GameManager.instance.player.transform.position;
+		Vector3 myPos = transform.position  ;
+
+
+		//플레이어가 어떻게 사용하느냐에 따라 예외사항이 발생할 수 있어  사용하지 않음
+		// Vector3 playerDir= GameManager.instance.player.inputVec;
+
+
+		switch (transform.tag)
+		{
+			case "Ground":
+		
+				float diffX = playerPos.x - myPos.x; 
+				float diffY = playerPos.y - myPos.y;
+				float dirX = diffX < 0 ? -1 : 1;
+				float dirY = diffY < 0 ? -1 : 1;
+
+				diffX = Mathf.Abs(diffX);
+				diffY = Mathf.Abs(diffY);
+
+				if (diffX > diffY)
+				{
+					transform.Translate(Vector3.right * dirX * 40);
+				}
+				else if(diffX < diffY)
+				{
+					transform.Translate(Vector3.up* dirY * 40);
+				}
+				break;
+
+			case "Enemy":
+				if (coll.enabled)
+				{
+					Vector3 dist = playerPos - myPos;
+					Vector3 ran = new Vector3(Random.Range(-3,3), Random.Range(-3,3), 0);
+					transform.Translate(ran + dist*2);
+										}
+				break;
+
+			default:
+				break;
+		}
+		}
 }
 
